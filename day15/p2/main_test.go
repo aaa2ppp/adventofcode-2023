8903c50398a9be04cb0cae291d5fac384a2fc7fb
Alex Pa <aaa2ppp@gmial.com>
2023-12-16 02:36:09 +0300

    day15 done

---
+++ day15/p2/main_test.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
+
+func Test_run(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantW   string
+		wantErr bool
+		debug   bool
+	}{
+		{
+			"1",
+			args{strings.NewReader(`rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`)},
+			`145`,
+			false,
+			true,
+		},
+		// {
+		// 	"2",
+		// 	args{strings.NewReader(``)},
+		// 	``,
+		// 	false,
+		// 	true,
+		// },
+		// {
+		// 	"3",
+		// 	args{strings.NewReader(``)},
+		// 	``,
+		// 	false,
+		// 	true,
+		// },
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			debugEnable = tt.debug
+			defer func() { debugEnable = false }()
+			w := &bytes.Buffer{}
+			if err := run(tt.args.r, w); (err != nil) != tt.wantErr {
+				t.Errorf("run() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotW := w.String(); strings.TrimSpace(gotW) != strings.TrimSpace(tt.wantW) {
+				t.Errorf("run() = %v, want %v", gotW, tt.wantW)
+			}
+		})
+	}
+}
+
+func Test_hash(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+
+		{"HASH", args{"HASH"}, 52},
+		// rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+		{"rn", args{"rn"}, 0},
+		{"cm", args{"cm"}, 0},
+		{"qp", args{"qp"}, 1},
+
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hash(tt.args.text); got != tt.want {
+				t.Errorf("hash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
